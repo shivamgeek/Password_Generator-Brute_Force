43c8ddc5e382d5374fa30bd7e087005e0a27aa93
ShivamGeek <shivamgeek@gmail.com>
2018-07-30 23:51:04 +0530

    Update brute_force.cpp

--- brute_force.cpp
+++ brute_force.cpp
@@ -5,20 +5,20 @@ bool generateCombinations(vector<char> &source,vector<char> &key,vector<char> &c
 	static long long count=0;
 	if(current.size()==key.size()){
 		cout<<++count<<" : ";
-		for(int i=0;i<key.size();i++){
+		for(unsigned int i=0;i<key.size();i++){
 			cout<<current[i];
 		}
 		cout<<endl;
 		if(key==current){
+			cout<<"MATCH FOUND!"<<endl;
 			return true;
 		}
 		return false;
 	}
-
-	for(int i=0;i<source.size();i++){
+	for(unsigned int i=0;i<source.size();i++){
 		current.push_back(source[i]);
 		if(generateCombinations(source,key,current)==true){
-			break;
+			return true;
 		}
 		current.pop_back();
 	}
@@ -29,18 +29,19 @@ bool generateCombinations(vector<char> &source,vector<char> &key,vector<char> &c
 
 int main(){
 	vector<char> source; // VECTOR TO STORE ALL POSSIBLE CHARACTERS THAT NEEDS TO BE BRUTE FORCED.
-	for(int i=0;i<26;i++){ 
+	for(int i=0;i<26;i++){
 		source.push_back('a'+i);
 	}
 
 	vector<char> current;
 	vector<char> key;
-	string s="abcde"; //INPUT KEY HERE
-  
-	for(int i=0;i<s.length();i++){
+	string s; //INPUT KEY HERE
+	cout<<"Enter key to be searched here"<<endl;
+	cin>>s;
+	for(unsigned int i=0;i<s.length();i++){
 		key.push_back(s[i]);
 	}
-  
+
 	if(key.size()>source.size()){  //IF INPUT KEY SIZE IF GREATER THAN POSSIBLE CHARACTERS.
 		cout<<"Key too long, NOT FOUND!"<<endl;
 	}
